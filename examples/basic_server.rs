0a34b2a7480e62edff20c3f47d37628240693de1
Steven Enamakel <enamakel@tinyhumans.ai>
2026-07-10 16:55:34 -0700

    update poitners

--- examples/basic_server.rs
+++
@@ -1,8 +0,0 @@
-use opencompany::{AppConfig, AppState};
-
-#[tokio::main]
-async fn main() -> opencompany::Result<()> {
-    let state = AppState::new(AppConfig::default());
-    println!("{}", serde_json::to_string_pretty(&state.spec()).unwrap());
-    Ok(())
-}
